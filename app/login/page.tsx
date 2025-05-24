55c9d53191268a7c6709bb5f2f496bc800100065
Matt Huang <wenliangmatt@gmail.com>
2025-05-24 22:58:35 +0800

    modified file

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ export default function Login() {
       //const response = await axios.post("https://agentic-ai-matt-backend.vercel.app/api/login", { account, password });
       const response = await axios.post("http://localhost:3000/api/login", { account, password });
       console.log(response.data);
-      if (response.data.success) {
+      if (response.data.message === "Login successful") {
         setResult("success");
       } else {
         setResult("fail");
